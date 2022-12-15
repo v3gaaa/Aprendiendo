72065cb871a4f415e3b40349b7f02831734f21e4
Sebastian Denhi Vega Saint Martin <104808082+v3gaaa@users.noreply.github.com>
2022-12-15 22:49:12 +0000

    commit

--- Cursos/Python/Proyecto Tienda Computadoras/Orden.py
+++ Cursos/Python/Proyecto Tienda Computadoras/Orden.py
@@ -17,11 +17,22 @@ class Orden:
     def imprimirOrden(self):
         strOrden = ""
         for i in range(len(self.listaOrden)):
-            strOrden += f"\n {self.listaOrden[i]}"
+            strOrden += f"\n \n {self.listaOrden[i]}"
         return strOrden
 
-    #def __str__(self):
-        #strOrden = self.imprimirOrden
-        #return strOrden
-            
-            
+           
+if __name__ == "__main__":
+    raton1 = Raton("USB","HP")
+    teclado1 = Teclado("Inalambrico","Gamer")
+    monitor1 = Monitor("27 pulgadas","LG")
+    compu1 = Computadora("Computadora Gamer", monitor1, raton1, teclado1)
+    
+    raton2 = Raton("USB","Logitech")
+    teclado2 = Teclado("USB","HP")
+    monitor2 = Monitor("30 pulgadas","Samsung")
+    compu2 = Computadora("Computadora Diseño", monitor2, raton2, teclado2)
+    
+    orden = Orden()
+    orden.agregar_computadora(compu1)
+    orden.agregar_computadora(compu2)
+    print(orden.imprimirOrden())
